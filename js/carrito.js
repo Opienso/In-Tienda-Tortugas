9855fc35db5419dbb5a5595d2617e9bc534ef39a
Opienso <ezequiel2001uni@gmail.com>
2022-12-05 18:44:09 -0300

    hehe

--- js/carrito.js
+++ js/carrito.js
@@ -1,37 +1,64 @@
-carritoLS = localStorage.getItem("carrito")
 
-carritoJSON = JSON.parse(carritoLS)
+carritoJSON = JSON.parse(localStorage.getItem("carrito"))
 
 ////////////////////////////////
 const contenedorCarrito = document.querySelector(".contenedor-carrito")
+const contenedorProductos = document.querySelector(".carrito-productos")
 const carritoAcciones = document.querySelector(".carrito-acciones")
 const MensajeCarritoVacio = document.querySelector(".carrito-vacio")
 const VaciarCarrito = document.querySelector(".carrito-acciones-vaciar")
+let RemoverProducto = document.querySelectorAll(".carrito-producto-eliminar")
+const Total = document.querySelector("#total")
+const comprar  = document.querySelector(".carrito-acciones-comprar")
 
-////////////////////////////////
-if(carritoJSON.length != 0){
-    carritoAcciones.classList.remove("disabled")
-    MensajeCarritoVacio.classList.add("disabled")
-}else{
-    carritoAcciones.classList.add("disabled")
-    MensajeCarritoVacio.classList.remove("disabled")
-}
+////////////////////////////////-----------------//////////////////////////////
 
-////////////////////////////////
 cargarProductos();
 
-function cargarProductos () {
-carritoJSON.forEach(producto => {
-    const productoAgregado = document.createElement("div")
-    productoAgregado.classList.add("carrito-producto")
-    productoAgregado.innerHTML = `<img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+
+VaciarCarrito.addEventListener("click", vaciarCarro)
+
+NumeritoQueVaAhiEnElCosoEseQueEstaAlLadoDelBotonDeComprarComoSeLlamabaEeeee();
+
+comprar.addEventListener("click", () => {
+    Swal.fire({
+        title: 'Realizado!',
+        text: 'Gracias por tu compra!',
+        imageUrl: './img/feliz.gif',
+        imageWidth: 270,
+        imageHeight: 429,
+        imageAlt: 'Custom image',
+        background: '#184241',
+        color: '#f4af14',
+        confirmButtonColor: '#8f8059',
+        confirmButtonText: 'De nada qwq',
+    })
+    vaciarCarro()
+})
+
+
+///////////////////////////////--------------------//////////////////////////
+function cargarProductos() {
+
+    if (carritoJSON != null) {
+
+        carritoAcciones.classList.remove("disabled")
+        MensajeCarritoVacio.classList.add("disabled")
+
+        let index = 0; 
+
+        carritoJSON.forEach(producto => {
+            const productoAgregado = document.createElement("div")
+            productoAgregado.classList.add("carrito-producto")
+            productoAgregado.classList.add(`index${index}`)
+            productoAgregado.innerHTML = `<img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
     <div class="carrito-producto-titulo">
         <small>${producto.titulo}</small>
         <h3>${producto.id}</h3>
     </div>
     <div class="carrito-producto-cantidad">
         <small>Cantidad</small>
-        <p>1</p>
+        <p>${producto.cantidad}</p>
     </div>
     <div class="carrito-producto-precio">
         <small>Precio</small>
@@ -39,51 +66,82 @@ carritoJSON.forEach(producto => {
     </div>
     <div class="carrito-producto-subtotal">
         <small>Subtotal</small>
-        <p>${producto.precio}</p>
+        <p>${producto.subtotal}</p>
     </div>
     <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash3"></i>Eliminar</button>`
-    
-    contenedorCarrito.append(productoAgregado)
-});
+
+
+            contenedorCarrito.prepend(productoAgregado)
+            index++
+        });
+    } else {
+
+        carritoAcciones.classList.add("disabled")
+        MensajeCarritoVacio.classList.remove("disabled")
+    }
+    actualizarBotonesEliminar();
 }
 
-////////////////////////////////
-VaciarCarrito.addEventListener("click", vaciarCarro)
+///////////////////////////
 
-function vaciarCarro (){
-    let quitarProducto = document.querySelectorAll(".carrito-producto")
+function actualizarBotonesEliminar(){
+    RemoverProducto = document.querySelectorAll(".carrito-producto-eliminar");
 
-    quitarProducto.forEach(producto => {
-        producto.remove();
-
-        localStorage.clear()
-        location.reload();
-})}
-
-function vaciarCarro2 (){
-    let quitarProducto = document.querySelectorAll(".carrito-producto")
-
-    quitarProducto.forEach(producto => {
-        producto.remove();
-
-        localStorage.setItem("carrito", JSON.stringify(carritoJSON))
-})}
-/////////////////////////////
-const RemoverProducto = document.querySelectorAll(".carrito-producto-eliminar")
-
-RemoverProducto.forEach(boton => {
-    boton.addEventListener("click", (e) => {
-        const caca = e.currentTarget.id
-
-        let quitarCarrito = carritoJSON.find((el)=> el.id === caca)
-
-        indice = carritoJSON.indexOf(quitarCarrito)
-
-        carritoJSON.splice(indice, 1)
-
-        vaciarCarro2();
-        cargarProductos();
-        location.reload();
+    RemoverProducto.forEach(boton => {
+        boton.addEventListener("click", eliminarProducto)
     })
-});
+}
+///////////////////////////
+
+function eliminarProducto(e){
+    if (carritoJSON.length === 1 && carritoJSON[0].cantidad === 1) { //Esto lo hago porque sino no se actualiza el msg no sé por q
+        vaciarCarro();
+    } else {
+        const ID = e.currentTarget.id;
+        const index = carritoJSON.findIndex(producto => producto.id === ID)
+
+        if (carritoJSON[index].cantidad > 1) {
+            carritoJSON[index].cantidad--
+            carritoJSON[index].subtotal = carritoJSON[index].subtotal - carritoJSON[index].precio
+
+            cargarProductos();
+
+            localStorage.setItem("carrito", JSON.stringify(carritoJSON))
+            location.reload();
+        } else {
+            const remover = document.querySelector(`.index${index}`)
+            console.log(remover)
+            remover.remove()
+
+            carritoJSON.splice(index, 1);
+
+            localStorage.setItem("carrito", JSON.stringify(carritoJSON))
+
+            location.reload()
+        }
+        NumeritoQueVaAhiEnElCosoEseQueEstaAlLadoDelBotonDeComprarComoSeLlamabaEeeee();
+    }
+}
+
+///////////////////////////
+function vaciarCarro() {
+    let quitarProducto = document.querySelectorAll(".carrito-producto")
+
+    quitarProducto.forEach(producto => {
+        producto.remove();
+    })
+    localStorage.clear()
+
+    carritoAcciones.classList.add("disabled")
+    MensajeCarritoVacio.classList.remove("disabled")
+}
+////////////////////////////
+function NumeritoQueVaAhiEnElCosoEseQueEstaAlLadoDelBotonDeComprarComoSeLlamabaEeeee(){
+    let total = 0
+
+    carritoJSON.forEach(producto => {
+        total = total + producto.subtotal
+    })
     
+    Total.innerText = "$" + total 
+}
--- js/main.js
+++ js/main.js
@@ -1,120 +1,10 @@
 //// OBJETOS - productos
 
-const productos = [
-    //abrigos
-    {
-        id: "abrigo-01",
-        titulo: "Sombrero fachero",
-        imagen: "./img/abrigos/01.jpg",
-        categoria: {
-            nombre: "Abrigos",
-            id: "abrigos",
-        },
-        precio: 2000
-    },
-    {
-        id: "Abrigo-02",
-        titulo: "Gallina",
-        imagen: "./img/abrigos/02.jpg",
-        categoria: {
-            nombre: "Abrigos",
-            id: "abrigos",
-        },
-        precio: 1500
-    },
-    {
-    id: "Abrigo-03",
-    titulo: "Florcita",
-    imagen: "./img/abrigos/03.jpg",
-    categoria: {
-        nombre: "Abrigos",
-        id: "abrigos",
-    },
-    precio: 1000
-    },
-    {
-        id: "Abrigo-04",
-        titulo: "Palmera",
-        imagen: "./img/abrigos/04.jpg",
-        categoria: {
-            nombre: "Abrigos",
-            id: "abrigos",
-        },
-        precio: 1700
-    },
-    {
-        id: "Abrigo-05",
-        titulo: "Cumpleaños",
-        imagen: "./img/abrigos/05.jpg",
-        categoria: {
-            nombre: "Abrigos",
-            id: "abrigo",
-        },
-        precio: 2500
-    },
-    //jeans
-    {
-        id: "Jean-01",
-        titulo: "Jean original",
-        imagen: "./img/pantalones/01.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones",
-        },
-        precio: 2000
-    },
-    {
-        id: "Jean-02",
-        titulo: "Jean trucho",
-        imagen: "./img/pantalones/02.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones",
-        },
-        precio: 1000
-    },
-    //armamento
-    {
-        id: "Armamento-01",
-        titulo: "Tortuga ninja",
-        imagen: "./img/armamento/01.jpg",
-        categoria: {
-            nombre: "Armamento",
-            id: "armamento",
-        },
-        precio: 3000
-    },
-    {
-        id: "Armamento-02",
-        titulo: "Casco",
-        imagen: "./img/armamento/02.jpg",
-        categoria: {
-            nombre: "Armamento",
-            id: "armamento",
-        },
-        precio: 2000
-    },
-    {
-        id: "Armamento-03",
-        titulo: "Tortuga cañon",
-        imagen: "./img/armamento/03.jpg",
-        categoria: {
-            nombre: "Armamento",
-            id: "armamento",
-        },
-        precio: 4000
-    },
-    {
-        id: "Armamento-04",
-        titulo: "Tortuga bombero",
-        imagen: "./img/armamento/04.jpg",
-        categoria: {
-            nombre: "Armamento",
-            id: "armamento",
-        },
-        precio: 3500
-    },
-]
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        cargarProductos(data);
+    })
 
 //// Mis variables con NODOS ewe...
 
@@ -122,15 +12,61 @@ const productos = [
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-menu")
 const TituloPrincipal = document.querySelector(".titulo-principal")
-const BotonTodosLosProductos = document.querySelector("#Todos")
-const BotonAbrigos = document.querySelector("#Abrigos")
-const BotonPantalones = document.querySelector("#Pantalones")
-const BotonArmamento = document.querySelector("#Armamento")
+const numeroCarrito = document.querySelector(".numerito")
 
 
-//// Cargo productos en la pag principal
+//// Esto hace que se resalten los botonsitos owo
 
-function cargarProductos() {
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        botonesCategorias.forEach(boton => boton.classList.remove("active")) ///Primero quitamos el active de TODOS! los botones
+        e.currentTarget.classList.add("active");  // Y cuando ninguno lo tiene se lo ponemos al que le hicimos click uwu
+
+        TituloPrincipal.innerText = e.currentTarget.id //Esto es para cambiar lo que dice el texto del main (?)
+
+        const productostos = document.querySelectorAll(".producto")
+        productostos.forEach(producto => producto.remove())
+
+        if (e.currentTarget.id != "Todos"){  //Aca estoy filtrando por el ID de lo que quiero cargar en el main
+            const filtrado = productos.filter(producto => producto.categoria.nombre.includes(e.currentTarget.id))
+            
+            filtrado.forEach(producto => {
+                const div = document.createElement("div");
+                div.classList.add("producto");
+                div.innerHTML = `
+                <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                <div class="producto-detalles">
+                    <h3 class="producto-titulo">${producto.titulo}</h3>
+                    <p class="producto-precio">$${producto.precio}</p>
+                    <button class="producto-agregar" id="${producto.id}">Agregar</button>
+                </div>
+                `;
+                contenedorProductos.append(div);
+            })
+            BotonAgregarCarrito();
+        }else{
+            cargarProductos()
+        }
+    })
+})
+
+
+const carrito = []
+
+carritoLS = localStorage.getItem("carrito")  //aca estoy obteniendo los datos del local storage que almacene dentro de la funcion BotonAgregarCarrito()
+carritoJSON = JSON.parse(carritoLS)
+
+if(carritoJSON != null){
+    carritoJSON.forEach(producto =>{
+        carrito.push(producto)
+        NumeritoCarrito();
+    })
+}else{
+    numeroCarrito.innerText = "0"
+}
+
+///////////////////////////////////////////
+function cargarProductos(productos) {
     productos.forEach(producto => {
 
         const div = document.createElement("div");
@@ -145,157 +81,10 @@ function cargarProductos() {
         `;
         contenedorProductos.append(div);
     })
-    BotonAgregarCarrito();
+    BotonAgregarCarrito(productos);
 }
-
-cargarProductos();
-
-//// Esto hace que se resalten los botonsitos owo
-
-botonesCategorias.forEach(boton => {
-    boton.addEventListener("click", () => {
-        botonesCategorias.forEach(boton => {
-            boton.classList.remove("active");  ///Primero quitamos el active de TODOS! los botones
-        })
-        boton.classList.add("active");  // Y cuando ninguno lo tiene se lo ponemos al que le hicimos click uwu
-    })
-})
-//////////////////////////
-
-BotonTodosLosProductos.addEventListener("click", ()=>{
-    TituloPrincipal.innerText = "Todos los productos"
-
-    const productos = document.querySelectorAll(".producto")
-
-    productos.forEach(producto => {
-        producto.remove();
-    })
-
-    cargarProductos();
-
-})
-
-//////////////////////////
-
-BotonAbrigos.addEventListener("click", () => {
-    let productos = document.querySelectorAll(".producto")
-
-    TituloPrincipal.innerText = "Abrigos"
-
-    productos.forEach(producto => {
-        producto.remove();
-    })
-
-    CargarAbrigos();
-    
-})
-
-
-function CargarAbrigos(){
-    const abrigos = productos.filter((producto) => producto.categoria.nombre.includes("Abrigos"));
-
-    abrigos.forEach(abrigo => {
-        const abriguitouwu = document.createElement("div")
-        abriguitouwu.classList.add("producto");
-        abriguitouwu.innerHTML = `
-        <img class="producto-imagen" src="${abrigo.imagen}" alt="${abrigo.titulo}">
-        <div class="producto-detalles">
-            <h3 class="producto-titulo">${abrigo.titulo}</h3>
-            <p class="producto-precio">$${abrigo.precio}</p>
-            <button class="producto-agregar" id="${abrigo.id}">Agregar</button>
-        </div>
-        `;
-
-        
-        contenedorProductos.append(abriguitouwu)
-    })
-    BotonAgregarCarrito();
-}
-
-///////////////////////
-
-BotonPantalones.addEventListener("click", ()=>{
-    let productos = document.querySelectorAll(".producto")
-
-    TituloPrincipal.innerText="Pantalones"
-
-    productos.forEach(producto => {
-        producto.remove();
-    })
-
-    CargarPantalones();
-})
-
-function CargarPantalones(){
-    const pantalones = productos.filter((producto) => producto.categoria.nombre.includes("Pantalones"));
-
-    pantalones.forEach(pantalon =>{
-        const pantalowo = document.createElement("div")
-        pantalowo.classList.add("producto");
-        pantalowo.innerHTML=`
-        <img class="producto-imagen" src="${pantalon.imagen}" alt="${pantalon.titulo}">
-        <div class="producto-detalles">
-            <h3 class="producto-titulo">${pantalon.titulo}</h3>
-            <p class="producto-precio">$${pantalon.precio}</p>
-            <button class="producto-agregar" id="${pantalon.id}">Agregar</button>
-        </div>
-        `;
-
-        
-        contenedorProductos.append(pantalowo)
-    })
-    BotonAgregarCarrito();
-}
-
-///////////////////
-
-BotonArmamento.addEventListener("click", ()=>{
-    let productos = document.querySelectorAll(".producto")
-
-    TituloPrincipal.innerText="Armamento"
-
-    productos.forEach(producto => {
-        producto.remove();
-    })
-
-    CargarArmamento();
-})
-
-function CargarArmamento(){
-    const Armamento = productos.filter((producto)=> producto.categoria.nombre.includes("Armamento"));
-
-    Armamento.forEach(arma => {
-        const armawa = document.createElement("div")
-        armawa.classList.add("producto")
-        armawa.innerHTML = `
-    <img class="producto-imagen" src="${arma.imagen}" alt="${arma.titulo}">
-    <div class="producto-detalles">
-        <h3 class="producto-titulo">${arma.titulo}</h3>
-        <p class="producto-precio">$${arma.precio}</p>
-        <button class="producto-agregar" id="${arma.id}">Agregar</button>
-    </div>
-    `;
-
-        contenedorProductos.append(armawa)
-    })
-    BotonAgregarCarrito();
-}
-/////////////////
-const carrito = []
-
-carritoLS = localStorage.getItem("carrito")
-
-carritoJSON = JSON.parse(carritoLS)
-
-if(carritoJSON != 0){
-    carritoJSON.forEach(producto =>{
-        carrito.push(producto)
-    })
-}else{
-    // nada jeje
-}
-
-function BotonAgregarCarrito() {
+///////////////////////////////////////////
+function BotonAgregarCarrito(productos) {
     let BotonAgregar = document.querySelectorAll(".producto-agregar")
 
     BotonAgregar.forEach(botonAgregar => {
@@ -303,11 +92,40 @@ function BotonAgregarCarrito() {
             const id = e.currentTarget.id
 
             let agregarAlCarrito = productos.find((el) => el.id === id);
-            
-            carrito.push(agregarAlCarrito)
+
+            if(carrito.some(el => el.id === id)){
+                const index = carrito.findIndex(producto => producto.id === id)
+                carrito[index].cantidad++;
+                carrito[index].subtotal = carrito[index].subtotal + carrito[index].precio;
+            }else{
+                agregarAlCarrito.cantidad = 1;
+                agregarAlCarrito.subtotal = agregarAlCarrito.precio;
+                console.log(agregarAlCarrito)
+                carrito.push(agregarAlCarrito)
+            }
+
+            NumeritoCarrito();
+
+            Toastify({
+
+                text: "Producto agregado al carrito :D",
+                gravity: "bottom",
+                position: "left",
+                className: "toast",
+                style: {
+                    background: "#8f8059"
+                },
+                destination: "./carrito.html",
+                duration: 2300
+                
+                }).showToast();
 
             localStorage.setItem("carrito", JSON.stringify(carrito))
         })
     })
 }
-
+//////////////////////////////////////////
+function NumeritoCarrito(){
+    numeritoCarrito = carrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numeroCarrito.innerText = numeritoCarrito
+}
